2cf5763c525c673be0322d997c48024dfd524c39
Tobias Koch <tkoch@anaconda.com>
2022-01-18 14:58:14 +0300

    Initial recipe

---
+++ recipe/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+git submodule init
+git submodule update
+
+mkdir .build && cd .build
+
+if [[ ${DEBUG_C} == yes ]]; then
+  CMAKE_BUILD_TYPE=Debug
+else
+  CMAKE_BUILD_TYPE=Release
+fi
+
+cmake \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
+    -DCMAKE_INSTALL_LIBDIR="lib" \
+    ..
+
+make install -j${CPU_COUNT} ${VERBOSE_CM}
