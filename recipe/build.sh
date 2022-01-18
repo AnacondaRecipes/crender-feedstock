0c3f4aebc20cd3823b53ec54a69259f243eb67c2
Tobias Koch <tkoch@anaconda.com>
2022-01-18 15:04:33 +0300

    Add back ${CMAKE_ARGS}

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,7 +11,7 @@ else
   CMAKE_BUILD_TYPE=Release
 fi
 
-cmake \
+cmake ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
     -DCMAKE_INSTALL_LIBDIR="lib" \
