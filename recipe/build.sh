16215df5780e1726812128e2f76f371a4eb836c1
Tobias Koch <tkoch@anaconda.com>
2022-01-19 17:38:01 +0300

    recipe/build.sh: work around jinja2cpp installation problem when in subfolder

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,10 @@
 git submodule init
 git submodule update
 
+# Dirty fixes.
+sed -i '/-Werror/d' CMakeLists.txt
+sed -i 's@jinja2cpp.pc.in jinja2cpp.pc@jinja2cpp.pc.in ${CMAKE_BINARY_DIR}/jinja2cpp.pc@g' Jinja2Cpp/CMakeLists.txt
+
 mkdir .build && cd .build
 
 if [[ ${DEBUG_C} == yes ]]; then
@@ -17,4 +21,5 @@ cmake ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_LIBDIR="lib" \
     ..
 
-make install -j${CPU_COUNT} ${VERBOSE_CM}
+make -j${CPU_COUNT} ${VERBOSE_CM}
+make install crender
