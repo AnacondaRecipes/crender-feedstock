c758929c28320874a58e5583ce02fcc15ce8ac72
Scott Bailey <Scott@TheBaileyFamily.org>
2022-01-20 15:34:18 -0700

    update tests

---
+++ recipe/run_test.bat
@@ -0,0 +1,5 @@
+
+
+REM Simple test to see that crender exists:
+crender -h
+if %errorlevel% neq 0 exit /b %errorlevel%
---
+++ recipe/run_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+# Simple test to see that crender exists:
+crender -h || exit -1
+
+
+# We should add some real test files here.
